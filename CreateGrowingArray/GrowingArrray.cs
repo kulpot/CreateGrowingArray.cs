314cbef656d829a8c477d02eda2c5c6b76c9b7c2
Kulpot <kulpotwahu@gmail.com>
2024-08-08 01:09:03 +0800

    ...

--- CreateGrowingArray/GrowingArrray.cs
+++ CreateGrowingArray/GrowingArrray.cs
@@ -40,7 +40,7 @@ namespace CreateGrowingArray
         {
             foreach(int x in intArray)
             {
-                if (x != 0)
+                if (x != 0)     // For display the rest to zeroes
                 {
                     Console.WriteLine(x);
                 }
